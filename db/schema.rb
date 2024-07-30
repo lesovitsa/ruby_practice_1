8bc22cca822db779af586482f1da80299f970f8c
Yana Mishkova-Kareva <yanamishkova@gmail.com>
2024-07-30 23:24:40 +0300

    Add user type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_07_30_194458) do
+ActiveRecord::Schema[7.2].define(version: 2024_07_30_202256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_07_30_194458) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "type"
     t.index ["email"], name: "index_users_on_email", using: :hash
     t.index ["user_id"], name: "index_users_on_user_id", using: :hash
   end
