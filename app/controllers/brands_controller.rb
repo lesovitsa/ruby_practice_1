18296b36290d6daafe735aed69be4edf93e011ef
Yana Mishkova-Kareva <yanamishkova@gmail.com>
2024-07-31 23:48:15 +0300

    Admin can register brands

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,2 +1,18 @@
 class BrandsController < ApplicationController
+    before_action :authorized_admin, only: [:register]
+
+    def register
+        @brand = Brand.create(brand_params)
+        if @brand.valid?
+            render json: {brand: @brand}
+        else
+            render json: {error: "Error registering brand"}
+        end
+    end
+
+    private
+
+    def brand_params
+        params.permit(:name, :state, :brand_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get "client_product/index"
-      get "client_product/show"
-      get "brand_product/index"
-      get "brand_product/show"
-      get "brand_aproduct/index"
-      get "brand_aproduct/show"
-      get "product/index"
-      get "product/show"
-      get "brand/index"
-      get "brand/show"
-      get "user/index"
-      get "user/show"
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -28,8 +16,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  # users
   resource :users, only: [:create]
   post "/register", to: "auths#create_client"
   post "/login", to: "auths#login"
   post "/register_admin", to: "auths#add_admin"
+
+  # brands
+  post "/brands/register", to: "brands#register"
 end
